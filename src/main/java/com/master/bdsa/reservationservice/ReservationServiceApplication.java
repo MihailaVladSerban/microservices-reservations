7e18740fe1d19d89179157931aaba350cd9c1e67
vlad <vlad@gmail.com>
2021-06-12 16:46:36 +0300

    updated

--- src/main/java/com/master/bdsa/reservationservice/ReservationServiceApplication.java
+++ src/main/java/com/master/bdsa/reservationservice/ReservationServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
-@SpringBootApplication
+/*@EnableFeignClients
+@SpringBootApplication*/
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
